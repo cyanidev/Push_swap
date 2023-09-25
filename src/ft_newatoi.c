aa456f2fcea98fd28bd82c4a1263156cc7fcce26
Andrea Feliciani <afelicia@c2r5s1.42madrid.com>
2023-09-25 16:13:36 +0200

    error when receiving '-' fixed

--- src/ft_newatoi.c
+++ src/ft_newatoi.c
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:22:56 by afelicia          #+#    #+#             */
-/*   Updated: 2023/09/22 02:12:56 by afelicia         ###   ########.fr       */
+/*   Updated: 2023/09/25 16:11:09 by afelicia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,14 @@ int	ft_newatoi(char *str)
 	sign = 1;
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
 		i++;
-	if (i == 0 && str[i] == '-') 
+	if (str[i] == '-') 
 	{
 		i++;
-		if (str[i] == '-' || str[i++] != '\0')
+		if (str[i] == '-' || (!ft_isdigit(str[i])))
 			ret_error("Error");
 		sign *= -1;
 	}
-	while (str[i] >= '0' && str[i] <= '9')
+	while (ft_isdigit(str[i]))
 	{
 		n = (str[i++] - '0') + (n * 10);
 		if (str[i] == '-')
